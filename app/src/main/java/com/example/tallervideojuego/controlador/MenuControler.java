f52b2f560bc9db8fccf2a23f23de42b2ff6f6ab5
JeanPaulCE <jpcdlalito@hotmail.com>
2022-09-23 11:13:27 -0600

    apartado grafico terminado

--- app/src/main/java/com/example/tallervideojuego/controlador/MenuControler.java
+++ app/src/main/java/com/example/tallervideojuego/controlador/MenuControler.java
@@ -14,48 +14,68 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.tallervideojuego.R;
 import com.example.tallervideojuego.controlador.bace.Controlador;
 import com.example.tallervideojuego.modelo.base.Entidad;
-import com.example.tallervideojuego.modelo.entidades.Carta;
 import com.example.tallervideojuego.modelo.entidades.Categoria;
 import com.example.tallervideojuego.modelo.registro.RegistroCategorias;
 import com.example.tallervideojuego.vista.Menu_act;
 
-import org.xmlpull.v1.XmlPullParser;
-
 import java.util.ArrayList;
 
 public final class MenuControler extends Controlador {
-    private Menu_act menu;
     private LinearLayout container;
     private LinearLayout[] personalizados;
     private Button add, bancoPreguntas, regresar;
     private RegistroCategorias registro;
 
     public MenuControler(AppCompatActivity act) {
-        menu = (Menu_act) act;
+        super(act);
         registro = RegistroCategorias.getInstance();
-        container = menu.findViewById(R.id.cat_container);
-        add = menu.findViewById(R.id.add);
-        regresar = menu.findViewById(R.id.regresar);
-        bancoPreguntas = menu.findViewById(R.id.bancoPreguntas);
+        container = this.act.findViewById(R.id.cat_container);
+        add = this.act.findViewById(R.id.add);
+        regresar = this.act.findViewById(R.id.regresar);
+        bancoPreguntas = this.act.findViewById(R.id.bancoPreguntas);
+        //____________________________________________________________
+        //____________________________________________________________
+        //____________________________________________________________
+        registro.add(new Categoria(0,"Prueba1"));
+        //____________________________________________________________
+        //____________________________________________________________
+        //____________________________________________________________
+
         addButtons();
         setFunctions();
     }
 
     private void addButtons() {
+
         ArrayList<Entidad> lista = registro.getEntidades();
 
         personalizados = new LinearLayout[lista.size()];
 
+        ContextThemeWrapper contextCat = new ContextThemeWrapper(act, R.style.btn_cat);
+        ContextThemeWrapper contextEdit = new ContextThemeWrapper(act, R.style.edit_btn);
+        ContextThemeWrapper contextLyt = new ContextThemeWrapper(act, R.style.lyt_personalizados);
 
-        LinearLayout.LayoutParams params_lyt = (LinearLayout.LayoutParams) menu.findViewById(R.id.lyt_personalizado).getLayoutParams();
-        LinearLayout.LayoutParams params_btn_cat = (LinearLayout.LayoutParams) menu.findViewById(R.id.btn_personalizado).getLayoutParams();
-        LinearLayout.LayoutParams params_btn_edit = (LinearLayout.LayoutParams) menu.findViewById(R.id.btnd_edit_personalizado).getLayoutParams();
+        LinearLayout.LayoutParams params_lyt =  new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        params_lyt.leftMargin = dp(30);
+        params_lyt.topMargin = dp(10);
+        params_lyt.rightMargin = dp(30);
+
+        LinearLayout.LayoutParams params_btn_cat = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params_btn_cat.width=0;
+        params_btn_cat.weight= (float) 0.65;
+        params_btn_cat.topMargin =dp(8);
+        params_btn_cat.bottomMargin = dp(8);
+
+
+        LinearLayout.LayoutParams params_btn_edit = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params_btn_edit.width=0;
+        params_btn_edit.weight= (float) 0.35;
+        params_btn_edit.topMargin = dp(8);
+        params_btn_edit.rightMargin = dp(8);
+        params_btn_edit.bottomMargin = dp(8);
 
 
 
-        ContextThemeWrapper contextCat = new ContextThemeWrapper(menu, R.style.btn_cat);
-        ContextThemeWrapper contextEdit = new ContextThemeWrapper(menu, R.style.edit_btn);
-        ContextThemeWrapper contextLyt = new ContextThemeWrapper(menu, R.style.lyt_personalizados);
 
         for (Entidad entidad:lista) {
             //obtiene los parametros de layaout de los botones ya definodos
@@ -85,12 +105,12 @@ public final class MenuControler extends Controlador {
             container.addView(personalizados[lista.indexOf(entidad)] );
         }
 
-        Toast.makeText(menu, "Prueba btns", Toast.LENGTH_SHORT).show();
+        Toast.makeText(act, "Prueba btns", Toast.LENGTH_SHORT).show();
     }
 
     private void setFunctions(){
         add.setOnClickListener(onAdd());
-        regresar.setOnClickListener(onRegresa());
+       // regresar.setOnClickListener(onRegresa());
         bancoPreguntas.setOnClickListener(onBancoPreguntas());
 
     }
@@ -99,22 +119,38 @@ public final class MenuControler extends Controlador {
         return new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(menu, "editar "+((Categoria)registro.search(id)).getTitulo(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(act, "editar "+((Categoria)registro.search(id)).getTitulo(), Toast.LENGTH_SHORT).show();
             }
         };
     }
 
     private View.OnClickListener onBancoPreguntas() {
-        return null;
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(act, "editar ", Toast.LENGTH_SHORT).show();
+            }
+        };
     }
 
     private View.OnClickListener onRegresa() {
-        return null;
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(act, "editar ", Toast.LENGTH_SHORT).show();
+            }
+        };
     }
 
     private View.OnClickListener onAdd() {
-        return null;
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(act, "editar ", Toast.LENGTH_SHORT).show();
+            }
+        };
     }
+
 
     public View.OnClickListener random(){
         return new View.OnClickListener() {
--- app/src/main/java/com/example/tallervideojuego/controlador/bace/Controlador.java
+++ app/src/main/java/com/example/tallervideojuego/controlador/bace/Controlador.java
@@ -1,6 +1,19 @@
 package com.example.tallervideojuego.controlador.bace;
 
-public class Controlador {
+import android.app.Activity;
+import android.util.TypedValue;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Controlador {
+    protected AppCompatActivity act;
+    public Controlador(AppCompatActivity act) {
+        this.act = act;
+    }
+
+
+    public int dp( int n){
+        return  ((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, act.getResources().getDisplayMetrics()))*n;
+    }
 
 }
